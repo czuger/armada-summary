6da2726a9c802d2466cd48cc6f799e12b500ca54
ced <zuger.cedric@gmail.com>
2019-12-15 15:52:34 +0100

    Processing ships.

--- run/process_data.rb
+++ run/process_data.rb
@@ -1,66 +1,37 @@
-require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'fileutils'
 require 'pp'
 require 'yaml'
 
-OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-APPLICATION_NAME = 'Google Sheets API Ruby Quickstart'.freeze
-CREDENTIALS_PATH = 'credentials.json'.freeze
-# The file token.yaml stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-TOKEN_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
+data = YAML.load_file( '../data/data.yaml' )
 
-##
-# Ensure valid credentials, either by restoring from the saved credentials
-# files or intitiating an OAuth2 authorization. If authorization is required,
-# the user's default browser will be launched to approve the request.
-#
-# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-def authorize
-  client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
-  token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
-  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
-  user_id = 'default'
-  credentials = authorizer.get_credentials user_id
-  if credentials.nil?
-    url = authorizer.get_authorization_url base_url: OOB_URI
-    puts "Open the following URL in the browser and enter the " \
-         "resulting code after authorization:\n" + url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI
-    )
+def process_ships( data )
+  ship_array = []
+  data.each do |ship_data|
+
+    ship = {}
+    ship[ :name ] = ship_data.shift.gsub( "\n", ' ' )
+    next if ship[ :name ] == ''
+
+    ship[ :veteran ] = ship_data.pop
+    ship[ :scarred ] = ship_data.pop
+    ship[ :flagship ] = ship_data.pop
+    ship[ :cost ] = ship_data.pop
+    ship[ :upgrades ] = []
+
+    until ship_data.empty?
+      data = ship_data.shift
+
+      next unless data
+      next if data == ''
+      ship[ :upgrades ] << data.gsub( "\n", ' ' )
+    end
+
+    ship_array << ship
   end
-  credentials
+  ship_array
 end
 
-# Initialize the API
-service = Google::Apis::SheetsV4::SheetsService.new
-service.client_options.application_name = APPLICATION_NAME
-service.authorization = authorize
-
-# Prints the names and majors of students in a sample spreadsheet:
-# https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-spreadsheet_id = '1O2KIwiN-_KcSo3777loMsjhZskw-zLUAkJ0xSxPIk7M'
-
-data = {}
-players = YAML.load_file( '../data/players_list.yaml' )
-
-p players
-
-players.each do |sheet|
-
-  range = "#{sheet}!B6:W17"
-  response = service.get_spreadsheet_values spreadsheet_id, range
-  data[ sheet ] = { ships: response.values }
-
-  range = "#{sheet}!B21:G44"
-  response = service.get_spreadsheet_values spreadsheet_id, range
-  data[ sheet ][ :squadrons ] = response.values
+data.each do |k, v|
+  v[ :ships ] = process_ships( v[ :ships ] )
 end
 
-File.open( '../data/data.yaml', 'w' ){ |f| f.write( data.to_yaml ) }
+pp data
