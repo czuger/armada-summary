e8487a3bb4889919797ed646a083f7d2f2221d1e
ced <zuger.cedric@gmail.com>
2019-12-16 20:33:55 +0100

    Print fleets in English and French.

--- run/libs/custom_i18n.rb
+++ run/libs/custom_i18n.rb
@@ -1,14 +1,17 @@
 require 'i18n'
 
+LOCALE_FR_FILE = '../locale/fr.yml'
+
 I18n.load_path = Dir['../locale/*.yml']
 I18n.config.available_locales = [ :en, :fr ]
 I18n.backend.load_translations
 
 def add_fr_translation( translation )
-  tr = YAML.load_file( LOCALE_FILE )
+  tr = YAML.load_file( LOCALE_FR_FILE )
 
   tr[ 'fr' ] ||= {}
+  tr[ 'fr' ][ :expansions ] ||= {}
+  tr[ 'fr' ][ :expansions ][ translation ] = translation
 
-  tr[ 'fr' ][ translation ] = translation
-  File.open( LOCALE_FILE, 'w' ){ |f| f.write( tr.to_yaml ) }
+  File.open( LOCALE_FR_FILE, 'w' ){ |f| f.write( tr.to_yaml ) }
 end
--- run/print_fleets.rb
+++ run/print_fleets.rb
@@ -1,8 +1,11 @@
 require 'yaml'
-
 require 'pp'
+require_relative 'libs/custom_i18n'
+require_relative 'libs/options'
 
 data = YAML.load_file( 'data/processed_data.yaml' )
+
+p I18n.locale
 
 data.each do |player, fleet|
   puts '*' * 150
@@ -12,16 +15,18 @@ data.each do |player, fleet|
   fleet[:ships].sort_by {|ship| [ ship[ :flagship ], ship[ :scarred ], ship[ :veteran ] ].join( '' ) }.reverse.each do |ship|
 
     title = ( ship[ :title ] ? " (#{ship[ :title ]})" : '' )
-    admiral = ( ship[ :flagship ] ? ' <Adm>' : '' )
+    admiral = ( ship[ :flagship ] ? I18n.t( 'fleet.adm' ) : '' )
     ship_print = [ ship[ :name ] + title + admiral ]
-    ship_print << (ship[ :scarred ] ? "Avarié ( #{ '%02d' % ( ship[ :ship_cost ] / 2.0 ).ceil } )" : '')
-    ship_print << (ship[ :veteran ] ? 'Vétéran' : '')
+    ship_print << (ship[ :scarred ] ? "#{I18n.t( 'fleet.scarred' )} ( #{ '%02d' % ( ship[ :ship_cost ] / 2.0 ).ceil } )" : '')
+    ship_print << (ship[ :veteran ] ? I18n.t( 'fleet.veteran' ) : '')
 
-    ship[ :upgrades ].each do |upgrade|
-      add_fr_translation( upgrade ) unless I18n.exists?(upgrade, :fr )
+    if I18n.locale == :fr
+      ship[ :upgrades ].each do |upgrade|
+        add_fr_translation( upgrade ) unless I18n.exists?(upgrade, :fr )
+      end
     end
 
-    ship_print << ship[ :upgrades ].map{ |upgrade| I18n.t( upgrade ) }.join( ', ' )
+    ship_print << ship[ :upgrades ].map{ |upgrade| ( I18n.locale == :fr ? I18n.t( "expansions.#{upgrade}" ) : upgrade ) }.join( ', ' )
 
     printf "%-43s%-14s%-8s%s\n" % ship_print
   end
@@ -31,9 +36,9 @@ data.each do |player, fleet|
   fleet[:squadrons].sort_by {|ship| [ ship[ :scarred ], ship[ :veteran ], ship[ :name ] ].join( '' ) }.reverse.each do |ship|
     ship_print = [ ship[ :name ] + ( ship[ :title ] ? " (#{ship[ :title ]})" : '' ) ]
     ship_print << (ship[ :scarred ] ? "Avarié ( #{ '%02d' % ( ship[ :cost ] / 2.0 ).ceil } )" : '')
-    ship_print << (ship[ :veteran ] ? 'Vétéran' : '')
+    ship_print << (ship[ :veteran ] ? I18n.t( 'fleet.veteran' ) : '')
 
-    ship_print << (ship[ :unique ] ? 'Unique' : '')
+    ship_print << (ship[ :unique ] ? I18n.t( 'fleet.unique' )  : '')
     ship_print << (ship[ :unique ] ? ship[ :unique_name ] : '')
 
     printf "%-43s%-14s%-8s%-7s%-20s\n" % ship_print
